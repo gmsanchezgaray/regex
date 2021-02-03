b3cf4f605c0fd27e754de88beee43dc7c5f87ff6
Gonza Sanchez <gmsanchezgaray@gmail.com>
2021-02-03 09:59:25 -0300

    checking the URL for Heroku

--- require/header.php
+++ require/header.php
@@ -256,6 +256,10 @@ if(isset($_SESSION['active'])){
                     </a>
                 </div>
             </li>
+
+            <li>
+                <?=  $url ?>
+            </li>
         </ul>
     </nav>
 </div>
